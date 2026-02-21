9154c6544f0df0ed72482ed582f696a6e6fe2fa2
kirillKozitskiy <kkozickij35@gmail.com>
2026-02-22 00:41:36 +0300

    rw-ticket-bot initial commit

---
+++ rw-parser-service/src/main/java/com/kozitskiy/rwticket/rwparserservice/RwParserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kozitskiy.rwticket.rwparserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RwParserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RwParserServiceApplication.class, args);
+	}
+
+}
---
+++ rw-parser-service/src/test/java/com/kozitskiy/rwticket/rwparserservice/RwParserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kozitskiy.rwticket.rwparserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RwParserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
